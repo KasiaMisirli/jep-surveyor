05fd3c96ba484ee1df15371e70ee85d57b1fd820
Kasia Misiri <kasia.misirli@travelport.com>
2019-02-26 16:49:05 +1100

    Add tests

---
+++ spec/requests/02_root_route_spec.rb
@@ -0,0 +1,11 @@
+
+RSpec.describe "GET /ratingQuestions" do
+  context "its getting all the results" do
+    it "returns a 200 OK" do
+      get "/ratingQuestions" 
+      expect(last_response.status).to eq(200)
+      body = JSON.parse(last_response.body)
+      expect(body.is_a?(Array)).to eq(true)
+    end
+  end
+end
