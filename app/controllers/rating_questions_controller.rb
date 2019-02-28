f055faed951fba707d93eb1bc229dbd66cb5bba7
Kasia Misiri <kasia.misirli@travelport.com>
2019-02-28 15:25:34 +1100

    all tests passing -pre refactoring

--- app/controllers/rating_questions_controller.rb
+++ app/controllers/rating_questions_controller.rb
@@ -1,24 +1,59 @@
+# require 'pry'
+
 class RatingQuestionsController < ApplicationController
+
   def index
+    # @rating_questions = RatingQuestion.all
     @rating_questions = RatingQuestion.all
+    @rating_questions.to_json
+    # render json: serialize_question(@rating_questions), status: 200
   end
 
   def new
     @rating_question = RatingQuestion.new
   end
-
-  def create
-    @rating_question = RatingQuestion.new(question_params)
-    if @rating_question.save
-      # flash[:notice] = "Your question has been created."
-      redirect_to "/", notice: "Your question has been created."
-    end
-
+  
+  def question_params
+    params.require(:rating_question).permit(:title, :tag)
   end
 
-  private
+  def serialize_question(question)
+    {
+      id: question.id.to_s,
+      title: question.title,
+      tag: question.tag
+    }
+  end
 
-  def question_params
-    params.require(:rating_question).permit(:title)
+  def create
+   if request.body.size.zero?
+    return render json: {}, status: 400
+   end
+    @rating_question = RatingQuestion.create(question_params)
+    if @rating_question.save
+      render json: serialize_question(@rating_question), status: 201
+    else
+      render json: {"errors"=>{"title"=>["can't be blank"]}}, status: 422
+    end
+  end
+
+  def destroy
+    question = RatingQuestion.find(params[:id])
+    return render json: {}, status: 404 unless question
+    question.destroy
+  end
+
+  def show
+    @rating_question = RatingQuestion.find(params[:id])
+    return render json: {}, status: 404 unless @rating_question
+    render json: serialize_question(@rating_question), status: 200
+    # binding.pry
+  end
+
+  def update
+    @rating_question = RatingQuestion.find(params[:id])
+    return render json: {}, status: 404 unless @rating_question
+    @rating_question.update(question_params)
+    render json: serialize_question(@rating_question), status: 200
   end
 end
--- spec/requests/02_root_route_spec.rb
+++ spec/requests/02_root_route_spec.rb
@@ -1,11 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "GET /ratingQuestions" do
+before do
+  post "/rating_questions.json", params: { rating_question: { title: "Hello World"} }
+  post "/rating_questions.json", params: { rating_question: { title: "Hello Mars"} }
+end
+
   context "its getting all the results" do
     it "returns a 200 OK" do
-      get "/ratingQuestions" 
-      expect(last_response.status).to eq(200)
-      body = JSON.parse(last_response.body)
+      get "/rating_questions.json" 
+      body = JSON.parse(response.body)
+      expect(response.status).to eq(200)
       expect(body.is_a?(Array)).to eq(true)
     end
   end
